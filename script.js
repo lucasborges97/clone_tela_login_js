877b7d0a205586c14bfe4b96c4a8a9b09993bb09
Lucas <lucasjborges.3@gmail.com>
2022-12-14 11:50:52 -0300

    Adição do projeto em construção

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function abrirModal() {
+    var modal = document.getElementById("modal");
+    modal.classList.remove("fechaModal");
+}
+
+function fechaModal() {
+    modal.classList.add("fechaModal");
+}
